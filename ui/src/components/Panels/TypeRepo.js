98430f9506bb029a825bd9d5dd77df2c73b7efcf
KrishnaPG <KrishnaPG@Yahoo.com>
2020-07-17 21:03:32 +0530

    impl: working on type sula

--- ui/src/components/Panels/TypeRepo.js
+++ ui/src/components/Panels/TypeRepo.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { Button, PageHeader } from 'antd';
+import importedComponent from 'react-imported-component';
+import 'jsoneditor-react/es/editor.min.css';
 
-import "json-schema-editor-visual/dist/main.css";
+const PlusCircleOutlined = React.lazy(() => import(/* webpackChunkName: "antIcons", webpackPreload: true */ '@ant-design/icons/PlusCircleOutlined'));
 
-import schemaEditor from "json-schema-editor-visual";
-const SchemaEditor = schemaEditor({});
+
+const JsonEditor = importedComponent(() => Promise.all([
+	import(/* webpackChunkName: "jsonEd", webpackPrefetch: true */ 'jsoneditor-react'),
+	import(/* webpackChunkName: "jsonEd", webpackPrefetch: true */ 'brace'),
+	import(/* webpackChunkName: "jsonEd", webpackPrefetch: true */ 'brace/mode/json'),
+	import(/* webpackChunkName: "jsonEd", webpackPrefetch: true */ 'brace/theme/github')
+]).then(([{ JsonEditor: Editor }, ace]) => {
+	return function EditorHoc(props) {
+		return (
+			<Editor
+				ace={ace}
+				theme="ace/theme/github"
+				{...props}
+			/>
+		);
+	}
+}), { async: true });
 
 class TypeRepo extends React.Component {
 
@@ -20,7 +38,34 @@ class TypeRepo extends React.Component {
 	}
 
 	render() {
-		return <SchemaEditor/>;
+		return <PageHeader
+			className="site-page-header"
+			title="Type Repository"
+			subTitle=""
+			extra={[
+				<Button key="3" icon={<PlusCircleOutlined />}>Add New Entry</Button>,
+				<Button key="2">Operation</Button>,
+				<Button key="1" type="primary">
+					Primary
+        </Button>,
+			]}			
+		/>		
+
+		// return <Suspense fallback={<div className="LoadingMsg">Loading the Editor...</div>}>
+		// 	<JsonEditor
+		// 		htmlElementProps={{class: "json-editor-container"}}
+		// 		name="Schepe"
+		// 		allowedModes={['tree', 'view', 'form', 'code', 'text']}
+		// 		mode='code'
+		// 		history={true}
+		// 		value={{}}
+		// 		onChange={this.handleChange}
+		// 	/>
+		// </Suspense>;
+	}
+
+	handleChange = ev => {
+		console.log("content changed: ", ev);
 	}
 };
 
