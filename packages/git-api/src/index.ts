fdcc9063649d4f4c91cce94b6d05c9b14d2c278a
Gopalakrishna Palem <4610831+KrishnaPG@users.noreply.github.com>
2025-08-20 11:34:35 +0530

    fix: error handling

--- packages/git-api/src/index.ts
+++ packages/git-api/src/index.ts
@@ -1,3 +1,4 @@
+import { swagger } from "@elysiajs/swagger"; // Example plugin
 import { Elysia } from "elysia";
 import { CONFIG } from "./config";
 import { gQLPlugin } from "./graphql/schema";
@@ -6,12 +7,27 @@ import { fileRoutes } from "./routes/file";
 import { repoRoutes } from "./routes/repo";
 
 const app = new Elysia()
-  .use(gQLPlugin as any)
+  .onError(({ code, error, set }) => {
+    switch (code) {
+      case "NOT_FOUND":
+        set.status = 404;
+        return { error: "Not Found" };
+      case "INTERNAL_SERVER_ERROR":
+        set.status = 500;
+        return { error: "Internal Server Error" };
+      default:
+        set.status = 500;
+        console.error(error);
+        return { error: "An unexpected error occurred" };
+    }
+  })
+  .use(swagger()) // Chains the Swagger plugin
+  .use(gQLPlugin)
   .use(repoRoutes)
   .use(fileRoutes)
   .use(commitRoutes)
   .get("/", ({ redirect }) => redirect("/graphql"))
-  .get("/health", () => "ok")
+  .get("/health", () => "{status: 'ok'}")
   .listen(CONFIG.PORT);
 
 console.log(`🚀 Server on http://localhost:${CONFIG.PORT}`);
